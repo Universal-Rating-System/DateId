5897d6fe61ba699e1e6591c6c71d636ac5d30724
Hendrik du Toit <hendrik@brightedge.co.za>
2025-03-01 14:45:07 +0200

    Configure Poetry for latest release

--- tests/test_dateid.py
+++ tests/test_dateid.py
@@ -11,22 +11,6 @@ from dateid import DateId
 
 
 class TestDateId:
-    # @pytest.mark.parametrize("base, today, day_id, month_id", test_set_01)
-    # def test_def_base_eq_today(self, base, today, day_id, month_id):
-    #     with time_machine.travel(datetime.date(today[0], today[1], today[2])):
-    #         base_date = datetime.date(base[0], base[1], base[2])
-    #         base_date_str = datetime.date(base[0], base[1], base[2]).strftime("%Y%m%d")
-    #         today_date = datetime.date.today()
-    #         today_date_str = datetime.date(today[0], today[1], today[2]).strftime("%Y%m%d")
-    #         date_id = DateId()
-    #         assert date_id.base_date == base_date
-    #         assert date_id.base_date_str == base_date_str
-    #         assert date_id.target_date == today_date
-    #         assert date_id.target_date_str == today_date_str
-    #         assert date_id.month_id == month_id
-    #         assert date_id.day_id.days == day_id
-    #     pass
-
     @pytest.mark.parametrize("base, today, day_id, month_id", test_set_01)
     def test_def_base_eq_today(self, base, today, day_id, month_id):
         # Create the datetime instance for travel; here using midnight.
@@ -48,22 +32,6 @@ class TestDateId:
 
         # Execute the decorated inner test.
         run_test()
-
-    @pytest.mark.parametrize("base, today, day_id, month_id", test_set_01)
-    def test_set_base_eq_today(self, base, today, day_id, month_id):
-        with time_machine.travel(datetime.date(today[0], today[1], today[2])):
-            base_date = datetime.date(base[0], base[1], base[2])
-            base_date_str = datetime.date(base[0], base[1], base[2]).strftime("%Y%m%d")
-            today_date = datetime.date.today()
-            today_date_str = datetime.date(today[0], today[1], today[2]).strftime("%Y%m%d")
-            date_id = DateId(p_base_date_str=base_date_str)
-            assert date_id.base_date == base_date
-            assert date_id.base_date_str == base_date_str
-            assert date_id.target_date == today_date
-            assert date_id.target_date_str == today_date_str
-            assert date_id.month_id == month_id
-            assert date_id.day_id.days == day_id
-        pass
 
     @pytest.mark.parametrize("base, target, day_id, month_id", test_set_02)
     def test_set_base_eq_target_str(self, base, target, day_id, month_id):
